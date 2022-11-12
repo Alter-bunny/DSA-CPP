16b122fdbd782a61c2c2891163b17459067f0365
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-11-12 08:39:58 +0530

    added rectangle.cpp

---
+++ patterns/rectangle.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int row, col;
+    cout << "Enter the number of rows:" << endl;
+    cin >> row;
+
+    cout << "Enter the number of columns:" << endl;
+    cin >> col;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+           cout<<"*";
+        }
+        cout << endl;
+    }
+}
+
+// for rows and column = 4
+// ****
+// ****
+// ****
+// ****
