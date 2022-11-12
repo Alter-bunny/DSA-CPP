c26911e00455df27a18b02081e23fedf74787ada
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-11-12 08:48:16 +0530

    number pyramid program

---
+++ patterns/number_pyramid.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows;
+    cout << "Enter Number of Rows(Steps): ";
+    cin >> rows;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < i+1; j++)
+        {
+            cout<<i+1;
+        }
+        cout<<endl;
+    }
+}
+
+// Enter Number of Rows(Steps): 5
+// 1
+// 22
+// 333
+// 4444
+// 55555
