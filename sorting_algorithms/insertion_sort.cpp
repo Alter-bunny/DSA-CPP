bc2c86b716541e6658dbdbe48a0f2a2152294b3d
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-12-01 08:32:55 +0530

    added insertion sort

---
+++ sorting_algorithms/insertion_sort.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void insertionSort(int arr[], int size)
+{
+    for (int i = 1; i < size; i++)
+    {
+        int j = i - 1, key = arr[i];
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int arr[] = {-34, 0, 5, 7, 1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, size);
+    printArray(arr, size);
+    return 0;
+}
